30004611baf8e3f55573b00c4229a923d4020aa1
Lukáš Klika <zax.drummer@gmail.com>
2025-10-15 10:41:23 +0200

    add headers for TgGame_Control

---
+++ src/GameServer/TgGame/TgGame_Control/TickCountdownCalculation/TgGame_Control__TickCountdownCalculation.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "src/pch.hpp"
+#include "src/Utils/HookBase.hpp"
+
+class TgGame_Control__TickCountdownCalculation : public HookBase<
+	void(__fastcall*)(ATgGame_Control*, void*, int),
+	0x10ad9bf0,
+	TgGame_Control__TickCountdownCalculation> {
+public:
+	static void __fastcall Call(ATgGame_Control* Game, void* edx, int nPriority);
+	static inline void __fastcall CallOriginal(ATgGame_Control* Game, void* edx, int nPriority) {
+		m_original(Game, edx, nPriority);
+	};
+};
+
+
