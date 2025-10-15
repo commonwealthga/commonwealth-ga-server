6b4354d69dbdf21e06e3286ef42be1e9165b2ea0
Lukáš Klika <zax.drummer@gmail.com>
2025-10-15 10:38:47 +0200

    add headers for TgGame_Arena

---
+++ src/GameServer/TgGame/TgGame_Arena/FinalizeGameScore/TgGame_Arena__FinalizeGameScore.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "src/pch.hpp"
+#include "src/Utils/HookBase.hpp"
+
+class TgGame_Arena__FinalizeGameScore : public HookBase<
+	void(__fastcall*)(ATgGame*, void*, int),
+	0x10ad9bf0,
+	TgGame_Arena__FinalizeGameScore> {
+public:
+	static void __fastcall Call(ATgGame_Arena* Game, void* edx, int nPriority);
+	static inline void __fastcall CallOriginal(ATgGame_Arena* Game, void* edx, int nPriority) {
+		m_original(Game, edx, nPriority);
+	};
+};
+
+
